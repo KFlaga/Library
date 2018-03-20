e53a98dbb8516ec2b4a9c31114496cf987f83c08
3HeadedMonkey <3HeadedMonkey@users.noreply.github.com>
2018-03-20 23:30:06 +0100

    Databases and account modules: search_for_books() and search_by_letter

--- account.py
+++ account.py
@@ -1,8 +1,31 @@
+import csv
+
 def search_for_books():
-    pass
+    """Ask for the type of search and then lists the books based of the criteria"""
+    type_of_search = 0
+    print("Do you want to search for books by the first letter of the title [A] or by the type [T]? ")
+    while type_of_search is not 'X':
+        type_of_search = input(">  ")
+        if type_of_search == 'A':
+            search_by_letter()
+        elif type_of_search == 'T':
+            search_by_type()
+        else:
+            print("Command unknown, write 'A' or 'T', to exit type 'X'")
+    return()
 
 def search_by_letter():
-    pass
+    """ Lists books that are starting with the entered letter"""
+    print("What is the first letter of the searched title? Use bigcase")
+    letter = input(">  ")
+    with open('books.csv', 'r') as book_base:
+        book_list = csv.reader(book_base)
+        next(book_list)
+
+        for line in book_list:
+            if startswith(letter):
+                print(line)
+    return()
 
 def search_by_type():
     pass
