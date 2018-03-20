bb5156faab2ae08e93dff706fc8520223b48e956
3HeadedMonkey <3HeadedMonkey@users.noreply.github.com>
2018-03-20 17:45:29 +0100

    Second commit

---
+++ Library.py
@@ -0,0 +1,24 @@
+class Person():
+    pass
+
+class Books():
+    pass
+
+class Logging():
+    pass
+
+class Account():
+    pass
+
+class Account_creation():
+    pass
+
+class Librarian():
+    def adding_books():
+        pass
+    def deleting_books():
+        pass
+    def person_search():
+        pass
+    def delet_account():
+        pass
---
+++ account.py
@@ -0,0 +1,26 @@
+def search_for_books():
+    pass
+
+def search_by_letter():
+    pass
+
+def search_by_type():
+    pass
+
+def check_my_books():
+    pass
+
+def rent_book():
+    pass
+
+def change_account_details():
+    pass
+
+def change_name():
+    pass
+
+def change_surname():
+    pass
+
+def change_password():
+    pass
