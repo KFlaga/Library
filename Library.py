c8653e90d568eec130c08faa3d98d09aa97c0ba0
3HeadedMonkey <3HeadedMonkey@users.noreply.github.com>
2018-03-26 14:59:45 +0200

    Added comments of classes Books and Person

--- Library.py
+++ Library.py
@@ -2,9 +2,24 @@
 
 class Person():
     pass
+# person is a database, nothing more than an account
+# unless we will do it a class that will take out person data!
+# And will call out persons books when needed
+# There should be also an information for the librarian who has the book
+# so that info should stay with the book?
+# Book shouldn't have all its info in one file as it complicates
+# adding books. There shoudl be corelated file that woudl keep that data
+# and book should have
 
-class Books():
-    pass
+class Books(object):
+
+    __init__(self,book):
+        self.book = book
+    # dectipts the csv file into data
+    # add junction between books.csv and users holding the books
+    # add multiple books of the same kind to the database and solve the
+    # problems associated
+    #
 
 class Logging():
     """ Gives user possibility to either log in or to create account"""
@@ -16,22 +31,20 @@ class Logging():
          )
     choice = input('>  ')
 
-    while choise is not '3'
+    while choise is not '3':
         if choice is '1':
-            login = log_in()
-            Account()
-
+            login = log_in() # login is users indicator
+            Account(login)
         elif choice is '2':
             create_account()
         else:
             print("Wrong comment, try again")
-    return "Logged" # Go to class account
+    print("Closing the system, goodbye")
+    
 
 class Account():
-    pass
+    pass # add a small self.__init__ function to do sth
 
-class AccountCreation():
-    pass
 
 class Librarian():
     def adding_books():
